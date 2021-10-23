93ead4b71ab2144f1eb2245d18673e354857b5e5
master <stathislo13@gmail.com>
2021-10-24 00:11:36 +0300

    Final Project0 && added Project1

---
+++ Project0-YS02/new/tests/dictionaries.py
@@ -0,0 +1,9 @@
+guests = ["Alice", "Bob", "Carol", "David" ]
+invites = [1] * 3 + [0]
+def is_invited(guest, inv, no):
+    if inv:
+        print(f"Guest {no}: Welcome, {guest}")
+    else:
+        print(f"Guest {no}: You are not invited, {guest}")
+for no, (guest, inv) in enumerate(zip(guests, invites)):
+    is_invited(guest, inv, no)
