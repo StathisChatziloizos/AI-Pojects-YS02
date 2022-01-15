490a8412a9809dc3f722a0731a355e9df00c19e6
master <stathislo13@gmail.com>
2022-01-15 05:59:21 +0200

    3 Constraints done, some more to go :)

--- Project3-YS03/scheduling.py
+++ Project3-YS03/scheduling.py
@@ -13,11 +13,11 @@ class Course:
         self.hasLab = hasLab_
 
         Course.totalCourses += 1
-        self.counter = Course.totalCourses
+        self.index = Course.totalCourses
 
 
     def print(self):
-        print(f"----------- Course({self.counter}) -----------")
+        print(f"----------- Course({self.index}) -----------")
         print(f"Semester: {self.semester}")
         print(f"Course Name: {self.courseName}")
         print(f"Professor: {self.professor}")
@@ -26,16 +26,24 @@ class Course:
         
 
 class Scheduling(csp.CSP):
+
+    inputFile = 'Στοιχεία Μαθημάτων.csv'
+    daysNum = 21
+
     def __init__(self):
         self.variables = []
         # Dictionary {variable : domain of that variable}
         self.domains = dict()
         # 2 variables are neighbors if the value of the one affects the other
         self.neighbors = dict()
+        self.courses = []
+
+        self.setVariables(Scheduling.inputFile, Scheduling.daysNum)
+        self.setDomain()
+        self.setNeighbors()
+        csp.CSP.__init__(self, self.variables, self.domains, self.neighbors, self.constraintFunction)
 
 
-    inputFile = 'Στοιχεία Μαθημάτων.csv'
-    daysNum = 21
 
 
     def readFile(inputFile):
@@ -45,11 +53,15 @@ class Scheduling(csp.CSP):
             csv_reader = csv.reader(csv_file)
 
             next(csv_reader)
+            # i = 0
             for item in csv_reader:
-                course = Course(item[0], item[1], item[2], item[3], item[4])
+                # i+=1
+                # if i > 5:
+                #     return
+                course = Course(int(item[0]), item[1], item[2], item[3], item[4])
                 courses.append(course)
                 if item[4] == 'TRUE':
-                    courseLab = Course(item[0], 'LAB_' + item[1], item[2], ' - ', ' - ')
+                    courseLab = Course(-1, 'LAB_' + item[1], item[2], ' - ', ' - ')
                     courses.append(courseLab)
 
         # for course in courses:
@@ -59,9 +71,9 @@ class Scheduling(csp.CSP):
 
     def setVariables(self,inputFile, daysNum):
         """ Variables of form (x,y) """
-        courses = Scheduling.readFile(inputFile)
-        for i in range(1,Course.totalCourses+1):
-            for j in range(1,daysNum+1):
+        self.courses = Scheduling.readFile(inputFile)
+        for i in range(1, Course.totalCourses+1):
+            for j in range(1, daysNum+1):
             #     print('-', end=' ')
             # print("\n")
                 self.variables.append((i,j))
@@ -70,7 +82,9 @@ class Scheduling(csp.CSP):
     def setDomain(self):
         """ Domain of form (M,N,A,-) --> (Morning, Noon, Afternoon, Empty) """
         for var in self.variables:
-            self.domains[var] = ['M','N','A', '-']
+            self.domains[var] = ['9-12','12-3','3-6', '-']
+            # self.domains[var] = [111,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43]
+            # self.domains[var] = ['M','N','A']
             # print(self.domains)
 
     def setNeighbors(self):
@@ -78,11 +92,53 @@ class Scheduling(csp.CSP):
             for i in range(1,Course.totalCourses+1):
                 if i != var[0]:
                     self.neighbors.setdefault(var,[]).append((i, var[1]))
+            for i in range(1, Scheduling.daysNum+1):
+                if i != var[1]:
+                    self.neighbors.setdefault(var,[]).append((var[0], i))
 
         # print(self.neighbors)
+
+    # i = course counter, j = day
+    def constraintFunction(self, A, a, B, b):
+        # For same column - same day
+        if A[1] == B[1]:
+            if a == b != '-':
+                return False
+
+            # Indexes of the courses corresponding to variables A and B
+            courseA_index = A[0] - 1
+            courseB_index = B[0] - 1
+
+            # Courses of the same semester can't be on the same day.
+            # Labs are obviously excluded from this constraint, hence semester > 0
+            # (labs' semesters are set by default to -1)
+            if self.courses[courseA_index].semester == self.courses[courseB_index].semester > 0:
+                # A and B cannot be both occupied
+                if a != '-' and b != '-':
+                    return False
+
+        # For same row - same course
+        if A[0] == B[0]:
+            if a == b != '-':
+                return False
+            if a == '9-12' or a == '12-3' or a == '3-6':
+                if b != '-':
+                    return False
+        
+        
+        return True
+        # return False
+
+
+    def displayVariables(self, daysNum):
+        for i in range(1, Course.totalCourses+1):
+            for j in range(1, daysNum+1):
+                print(self.curr_domains[(i,j)], end=' ')
+            print("\n")
         
 
 s1 = Scheduling()
-s1.setVariables(Scheduling.inputFile, Scheduling.daysNum)
-s1.setDomain()
-s1.setNeighbors()
+csp.backtracking_search(s1)
+# s1.display(s1.infer_assignment())
+s1.displayVariables(Scheduling.daysNum)
+# s1.display(s1.infer_assignment())
